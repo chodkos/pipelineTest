99cf5ccfe87d1c6cc6aff2c0479974de2cf8e9a9
Chodkowski <lukasz.chodkowski@accenture.com>
2018-01-23 17:08:47 +0100

    ERTY

--- src/test/java/simpleTest.java
+++ src/test/java/simpleTest.java
@@ -3,28 +3,40 @@ import org.testng.ITest;
 import org.testng.ITestResult;
 import org.testng.annotations.Test;
 
+import java.util.logging.Logger;
+
 public class simpleTest {
+
+    private final static Logger LOG = Logger.getLogger(simpleTest.class.getName());
 
     @Test
     public void calc(){
+        LOG.info("Logger msg here");
         Assert.assertEquals(2+2, 4, "Sth went very wrong");
         System.out.println(2+2);
+        LOG.info("Calc test passed");
     }
 
     @Test
     public void another(){
+        LOG.info("Inside another test");
         Assert.assertEquals(5+5, 10);
         System.out.println(5+5);
+        LOG.info("Sucess");
     }
 
     @Test
     public void shouldPass(){
+        LOG.info("Inside shouldPass test");
         Assert.assertTrue(true);
+        LOG.info("Sucess");
     }
 
     @Test
     public void shouldFail(){
-        Assert.assertTrue(true);
+        LOG.info("Inside shouldFail test");
+        Assert.assertTrue(false);
+        LOG.info("Should fail sometimes");
     }
 
 
