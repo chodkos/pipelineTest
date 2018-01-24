1e3a88bd15a078a6ab98583d1729496d6bc9e75b
Chodkowski <lukasz.chodkowski@accenture.com>
2018-01-24 10:45:27 +0100

    SAS

--- src/test/java/simpleTest.java
+++ src/test/java/simpleTest.java
@@ -35,7 +35,7 @@ public class simpleTest {
     @Test
     public void shouldFail(){
         LOG.info("Inside shouldFail test");
-        Assert.assertTrue(false);
+        Assert.assertTrue(true);
         LOG.info("Should fail sometimes");
     }
 
